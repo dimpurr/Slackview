21c5bd3386f64c69f64a6e45fb04b7faacc904b9
dimpurr <dimpurr@live.com>
2015-08-07 16:53:39 +0800

    V1.0 Widescreen support, zipped background and pingback modified.

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
 
 <footer class="footer" role="contentinfo">
 <p>Theme <a href="http://blog.dimpurr.com/slackview">Slackview</a> by <a href="http://dimpurr.com">Dimpurr</a></p>
-<p>Poundly powered by <a href="http://wordpress.org">WordPress</a></p>
+<p>Proudly powered by <a href="http://wordpress.org">WordPress</a></p>
 
 <?php wp_footer(); ?>
 
