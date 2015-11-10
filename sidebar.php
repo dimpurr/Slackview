4f00a2d752def8c93bd475eb0ccacf1f1a1d34e1
ccloli <h@ccloli.com>
2015-11-10 21:39:19 +0800

    修复登录用户顶栏下拉菜单难以点击（z-index）

--- sidebar.php
+++ sidebar.php
@@ -6,11 +6,10 @@
 <p>Theme <a href="http://blog.dimpurr.com/slackview">Slackview</a> by <a href="http://dimpurr.com">Dimpurr</a></p>
 <p>Proudly powered by <a href="http://wordpress.org">WordPress</a></p>
 
-<?php wp_footer(); ?>
-
 <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.sticky.js"></script>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/script.js"></script>
 </footer>
 
 </div>
+<?php wp_footer(); ?>
